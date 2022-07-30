ca71064f062cb25ff4d6505017db20bf3aa06bda
Atsushi.Narusawa <atsushi.narusawa@access-company.com>
2022-07-30 16:23:27 +0900

    [BE] `message_controller` のユニットテストを追加

---
+++ api/tests/message_controller/test_get_hello.py
@@ -0,0 +1,31 @@
+import json
+import azure.functions as func
+
+from message_controller.index import main
+from lib.models import Session, Message
+
+
+def test_returns_200():
+    with Session() as session:
+        entity = Message(id=1, content='test')
+        session.add(entity)
+        session.commit()
+
+    body = json.dumps({
+        'message': 'hello',
+    }).encode('utf-8')
+    route_params = {
+        'action': 'hello',
+        'id': '1',
+    }
+    req = func.HttpRequest(method='GET', body=None, route_params=route_params, url='')
+    resp = main(req)
+    resp_body = json.loads(resp.get_body())
+    expected = [
+        {'id': 1, 'content': 'test'}
+    ]
+
+    assert resp.status_code == 200
+    assert resp_body == expected
+
+
---
+++ api/tests/message_controller/test_post_hello.py
@@ -0,0 +1,25 @@
+import json
+import azure.functions as func
+
+from message_controller.index import main
+
+
+def test_returns_200():
+    body = json.dumps({
+        'message': 'hello from client',
+    }).encode('utf-8')
+
+    route_params = {
+        'action': 'hello',
+        'id': '1',
+    }
+
+    req = func.HttpRequest(method='POST', body=body, route_params=route_params, url='')
+    resp = main(req)
+    resp_body = json.loads(resp.get_body())
+    expected = {
+        'message': 'echo back `hello from client` from POST method.',
+    }
+
+    assert resp.status_code == 200
+    assert resp_body == expected
